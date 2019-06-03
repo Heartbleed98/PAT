b0f6dc4e2ecf00b9caa5df2ca6b09dd5050b5b81
Heartbleed <hhttiantian@outlook.com>
2019-06-03 14:04:26 +0800

    Modification

--- PAT甲级/1003.cpp
+++ PAT甲级/1003.cpp
@@ -1,26 +1,80 @@
-#include <iostream>
+#include<iostream>
+#include<string>
+#include<cstring>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+const int inf = 99999999;
+
+int matrix[510][510];
+bool visited[510];
+int dist[510];
+int path[510];
+int source;
+
+int num[510], w[510];
 int main()
 {
-    int z,n,max,sum;
-    int a,b,A,B,t; //a¡¢b¼ÇÂ¼µ±Ç°Á¬Ğø×ÓĞòÁĞµÄÆğÊ¼¡¢½áÊøÎ»ÖÃ
-    scanf("%d",&z);
-    for(int k=1;k<=z;k++)
-    {
-        scanf("%d",&n);
-        sum = max = -1001;
-        for(int i=1;i<=n;i++)
-        {
-            scanf("%d",&t);
-            if(sum+t < t) 
-                sum = t , a = b = i;     
-            else
-                sum += t , ++b;
-            if(max < sum)
-                max = sum , A = a , B = b;
-        }
-        printf("Case %d:\n%d %d %d\n",k,max,A,B);
-        if(k-z) puts("");
-    }
+	int n, m, c1, c2;
+	scanf("%d%d%d%d", &n,&m,&c1,&c2);
+	
+	vector<int> weight;
+	for(int i = 0; i < n; i++)
+	{
+		int tmp;
+		scanf("%d", &tmp);
+		weight.push_back(tmp);
+	}
+		
+	fill(matrix[0], matrix[0] + 510 * 510, inf);
+	fill(dist, dist + 510, inf);	
+	for(int i = 0; i < m; i++)
+	{
+		int a, b, c;
+		scanf("%d%d%d", &a,&b,&c);
+		matrix[a][b] = matrix[b][a] = c;	
+	}
+	
+	dist[c1] = 0;
+	w[c1] = weight[c1];
+	num[c1] = 1;
+
+	for (int i = 0; i < n; i++)
+	{
+		int min_cost = inf; //æœ€å°æƒå€¼
+		int min_cost_index = -1; //æœ€å°æƒå€¼çš„ä¸‹æ ‡
+		for (int j = 0; j < n; j++)
+		{
+			if (visited[j] == false && dist[j] < min_cost)
+			{
+				min_cost = dist[j];
+				min_cost_index = j;
+			}
+		}
+		if(min_cost_index == -1) break;
+		visited[min_cost_index] = true;
+		
+		for (int j = 0; j < n; j++)
+		{
+			if (visited[j] == false && matrix[min_cost_index][j] != inf)
+			{
+				if(matrix[min_cost_index][j] + min_cost < dist[j])
+				{
+					dist[j] = matrix[min_cost_index][j] + min_cost;
+					num[j] = num[min_cost_index];
+					w[j] = w[min_cost_index] + weight[j]; 
+				}
+				else if(matrix[min_cost_index][j] + min_cost == dist[j])
+				{
+					num[j] = num[j] + num[min_cost_index];
+					if(w[min_cost_index] + weight[j] > w[j])
+                        w[j] = w[min_cost_index] + weight[j];
+				}
+			}
+		}
+	}
+	
+	cout<<num[c2]<<" "<<w[c2]<<endl;
 	return 0;
 }
-
--- PAT甲级/1005.cpp
+++ PAT甲级/1005.cpp
@@ -1,41 +1,26 @@
-/*
-注意题目所给的n的范围，有可能n特别大
-fib函数的写法（动态规划）
-对7取余，得到的结果只有7种（7*7=49），可以根据此减少计算量
-*/ 
 #include<iostream>
 #include<string>
 #include<vector>
+#include<algorithm>
 using namespace std;
-
-int fib(int n, int a, int b)
-{
-	if(n == 1 || n == 2)
-		return 1;
-	int arr[50];
-	arr[1] = 1;
-	arr[2] = 1;
-	for(int i = 3; i <= 49; i++)
-	{
-		arr[i] = (a * arr[i-1] + b * arr[i-2]) % 7; 
-	}
-	return arr[n];
-}
 
 int main()
 {
-	int a, b, n;
-	while(cin>>a>>b>>n)
+	string input;
+	cin>>input;
+	
+	int sum = 0;
+	string words[10] = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+	for(int i = 0; i < input.size(); i++)
 	{
-		if(a == 0 && b == 0 && n == 0)
-			break;
-		else
-		{
-			n = n % 49;
-			cout<<fib(n, a, b)<<endl;
-		}
+		sum += (input[i] - '0');	
 	}
-
+	string s = to_string(sum);
+	for(int i = 0; i < s.size(); i++)
+	{
+		if(i != 0) cout<<" ";
+		cout<<words[s[i] - '0'];
+	}
+	
 	return 0;
 }
-
