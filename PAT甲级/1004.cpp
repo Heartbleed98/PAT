56d5a8969bcafb95885ae95e47c5fc30b8fee9f9
Heartbleed <hhttiantian@outlook.com>
2019-09-08 01:04:30 +0800

    UPDATE

--- PAT甲级/1004.cpp
+++ PAT甲级/1004.cpp
@@ -4,32 +4,48 @@
 #include<map>
 using namespace std;
 
+vector<int> level(102, 0);
+map<int, vector<int> >  m1;
+int d = 0;
+void dfs(int root, int depth)
+{
+	if(m1[root].size() == 0)
+	{
+		if(depth >= d) d = depth;
+		level[depth]++;
+		return;
+	}
+	else
+		for(int i = 0; i < m1[root].size(); i++)
+		{
+			int node = m1[root][i];
+			dfs(node, depth + 1);
+		}
+}
+
 int main()
 {
-	int num;
-	while(cin>>num)
+	int n, m;
+	scanf("%d%d", &n, &m);
+	
+	for(int i = 0; i < m; i++)
 	{
-		if(num == 0) break;
-		map<string, int> m1;
-		string str;
-		for(int i = 0; i < num; i++)
+		int id, k;
+		scanf("%d%d", &id,&k);
+		vector<int> child(k);
+		for(int j = 0; j < k; j++)
 		{
-			cin>>str;
-			m1[str]++;
+			scanf("%d", &child[j]);
 		}
-		int max = 0;
-		string max_color="";
-		map<string, int>::iterator it = m1.begin();
-		while(it != m1.end())
-		{
-			if(it->second > max)
-			{
-				max = it->second;
-				max_color = it->first;
-			}
-			it++;
-		}
-		cout<<max_color<<endl;
+		m1[id] = child;
+	}
+
+	dfs(1, 1);
+
+	for(int i = 1; i <= d; i++)
+	{
+		if(i != 1) printf(" ");
+		printf("%d", level[i]);
 	}
 
 	return 0;
