1af1fe76b3e03dc0b81eb34a6438478a75687f94
Heartbleed <hhttiantian@outlook.com>
2020-02-23 11:45:10 +0800

    UPDATE 甲级 1007

--- PAT甲级/1007.cpp
+++ PAT甲级/1007.cpp
@@ -1,19 +1,43 @@
-#include<iostream>
-#include<string>
-#include<vector>
-#include<algorithm>
+// 求最大子序列和，并输出该子序列的首元素和最后一个元素
+// （若最大子序列和为负数，则输出0，序列的第一个元素和最后一个元素
+#include <iostream>
+#include <vector>
+#include<math.h>
 using namespace std;
-
 int main()
 {
-	int k;
-	scanf("%d", &k);
-	
-	for(int i = 0; i < k; i++)
-	{
-		
-	}
+    int n;
+    scanf("%d", &n);
+    vector<int> nums(n);
+    for(int i = 0; i < n; i++)
+        scanf("%d", &nums[i]);
 
+    int currSum = nums[0], maxSum = nums[0];
+    int a, b, A, B;
+    for(int i = 1; i < nums.size(); i++)
+    {
+        if(nums[i] > currSum + nums[i])
+        {
+            a = b = i;
+            currSum = nums[i];
+        }
+        else
+        {
+            b++;
+            currSum = currSum + nums[i];
+        }
+
+        if(currSum > maxSum)
+        {
+            A = a;
+            B = b;
+            maxSum = currSum;
+        }
+    }
+    if(maxSum < 0)
+        printf("%d %d %d", 0, nums[0], nums[nums.size()-1]);
+    else
+        printf("%d %d %d", maxSum, nums[A], nums[B]);
+    
 	return 0;
 }
-
